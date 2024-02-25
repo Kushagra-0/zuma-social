62fdcdfd74e14f18499eccf53e427d3103065451
Kushagra-0 <kushagrashukla.dev@gmail.com>
2024-02-25 11:35:58 +0530

    Made some user routes

--- server/index.js
+++ server/index.js
@@ -7,7 +7,12 @@ import multer from "multer";
 import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
 import { fileURLToPath } from "url";
+import { register } from "module";
+import { register } from "./controllers/auth.js";
+
 
 // CONFIGURATIONS
 const __filename = fileURLToPath(import.meta.url);
@@ -23,6 +28,7 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
 
+
 // FILE STORAGE
 const storage = multer.diskStorage({
     destination: function(req, file, cb) {
@@ -34,10 +40,19 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage });
 
+
+// ROUTES WITH FILES
+app.post("/auth/register", upload.single("picture"), register);
+
+// ROUTES
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+
+
 // MONGOOSE SETUP
 const PORT = process.env.PORT || 6001;
 mongoose.connect(process.env.MONGO_URL)
 .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`))
 })
-.catch((err) => console.log(`${error} did not connect`))
+.catch((err) => console.log(`${err} did not connect`))
