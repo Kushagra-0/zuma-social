0e576ec5a48eabaadbfc6fcca55168a1dfc1f865
Kushagra-0 <kushagrashukla.dev@gmail.com>
2024-03-09 16:27:47 +0530

    Made a login and registeration system

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -39,8 +39,8 @@ const Navbar = () => {
     const primaryLight = theme.palette.primary.light;
     const alt = theme.palette.background.alt;
 
-    // const fullName = `${user.firstName} ${user.lastName}`;
-    const fullName = "Obama Don"
+    const fullName = `${user.firstName} ${user.lastName}`;
+    // const fullName = "Obama Don"
 
     return (
         <FlexBetween padding="1rem 6%" backgroundColor={alt}>
